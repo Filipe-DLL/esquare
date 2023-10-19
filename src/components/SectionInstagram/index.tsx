2cf4ac441c9cb75ebd189d33c23542d5df01ca05
Filipe <ff467447@gmail.com>
2023-10-19 18:03:27 -0300

    Correção do Revalidate

--- src/components/SectionInstagram/index.tsx
+++ src/components/SectionInstagram/index.tsx
@@ -1,19 +1,32 @@
-import InstaFeed from '@/components/SectionInstagram/InstaFeed'
-import axios from 'axios'
+import InstaFeed from './InstaFeed'
 
 export default async function SectionInstagram() {
   const token = process.env.NEXT_INSTA_TOKEN
   const fields = 'media_type,media_url,permalink'
 
-  const userUrl = `https://graph.instagram.com/me?fields=id,username&access_token=${token}`
-  const mediaUrl = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${token}`
+  const userUrl = await fetch(
+    `https://graph.instagram.com/me?fields=id,username&access_token=${token}`,
+    {
+      next: {
+        revalidate: 3600,
+      },
+    },
+  )
+  const mediaUrl = await fetch(
+    `https://graph.instagram.com/me/media?fields=${fields}&access_token=${token}`,
+    {
+      next: {
+        revalidate: 3600,
+      },
+    },
+  )
 
-  const user = await axios.get(userUrl)
-  const media = await axios.get(mediaUrl)
+  const user = await userUrl.json()
+  const media = await mediaUrl.json()
 
   return (
     <section className="animate__fadeIn animate__animated flex h-full flex-col items-center justify-start">
-      <InstaFeed user={user.data} media={media.data.data} />
+      <InstaFeed user={user} media={media.data} />
     </section>
   )
 }
