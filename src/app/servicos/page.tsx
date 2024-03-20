f461e2490b6f1af346067b7c9e7c75750274f0ff
Filipe <ff467447@gmail.com>
2024-03-20 14:54:48 -0300

    add imagens na pagina de servições

--- src/app/servicos/page.tsx
+++ src/app/servicos/page.tsx
@@ -1,143 +1,53 @@
-import Orçamento from '@/assets/SectionServiços/Orçamento.jpg'
-import PPCI from '@/assets/SectionServiços/Prevenção-e-Combate-a-Incêndio-(PPCI).jpg'
-import Eletrico from '@/assets/SectionServiços/Projeto-Elétrico.jpg'
-import Arquitetonico from '@/assets/SectionServiços/Projeto-arquitetônico.jpg'
-import Estrutural from '@/assets/SectionServiços/Projeto-estrutural.jpg'
-import Hidrossanitário from '@/assets/SectionServiços/Projetos-Hidrossanitário.png'
-
-import ServiçosPageItem from '@/components/PageServiços/PageServiçosItem'
+import { serviçosData } from "@/data/serviçosData"
+import { default as Image } from "next/image"
 
 export default function Page() {
   return (
-    <div className="animate__fadeIn animate__animated flex h-full min-h-screen flex-col items-center justify-center bg-gray-50">
-      <div className="">
-        <ServiçosPageItem
-          titulo={'Projeto Arquitetônico'}
-          imagem={Arquitetonico}
-        />
-        <div className="flex w-full flex-col items-center justify-center gap-6 p-6 text-black">
-          <p className="w-8/12 p-8 text-justify text-lg font-medium leading-relaxed lg:w-11/12 sm:p-0 2sm:text-xs">
-            O projeto arquitetônico é o primeiro projeto a ser concebido quando
-            se vai construir ou reformar sua casa ou empreendimento. Nele vão
-            estar contidas todas as informações acerca dos desejos do cliente,
-            do que pode ou não pode ter em seu projeto, além de outros detalhes
-            que possam vir a impactar a concepção do projeto.
-            <br />
-            <br />O projeto arquitetônico abrange várias etapas até a
-            apresentação do projeto executivo final, sendo possível prever nele
-            problemas de execução e facilitar a mesma, tornando-se essencial
-            para a garantia de qualidade e segurança na obra.
-          </p>
-        </div>
-      </div>
-
-      <div className="">
-        <ServiçosPageItem titulo={'Projeto Estrutural'} imagem={Estrutural} />
-        <div className="flex w-full flex-col items-center justify-center gap-6 p-6 text-black">
-          <p className="w-8/12 p-8 text-justify text-lg font-medium leading-relaxed lg:w-11/12 sm:p-0 2sm:text-xs">
-            O projeto estrutural é o dimensionamento e detalhamento da estrutura
-            necessária para sustentar a edificação, composta por vigas, pilares,
-            lajes e fundação. Por meio de critérios e cálculos específicos, e de
-            acordo com as normas vigentes, o engenheiro define qual o tipo de
-            estrutura mais adequado para o projeto, as dimensões e
-            especificações de cada elemento.
-            <br />
-            <br />É considerado o projeto mais importante de uma construção,
-            pois é ele que vai garantir a sustentação da edificação. Além de
-            evitar problemas como trincas, rachaduras, rebaixamento de piso,
-            etc.
-          </p>
-        </div>
-      </div>
-
-      <div className="">
-        <ServiçosPageItem titulo={'Projeto Elétrico'} imagem={Eletrico} />
-        <div className="flex w-full flex-col items-center justify-center gap-6 p-6 text-black">
-          <p className="w-8/12 p-8 text-justify text-lg font-medium leading-relaxed lg:w-11/12 sm:p-0 2sm:text-xs">
-            Um projeto elétrico residencial é o conjunto de todas as
-            informações, cálculos, desenhos e detalhes referentes à instalação
-            elétrica residencial, observando todas as normas nacionais vigentes.
-            Essas informações podem ser um diagrama elétrico, tabela de dados,
-            layout, desenhos técnicos, listagem dos componentes da instalação e
-            outros.
-            <br /> <br />O projeto elétrico se beneficia muito de um projeto
-            arquitetônico bem feito. Além de ser extremamente necessário para
-            proteção dos equipamentos ligados na casa, desde equipamentos mais
-            robustos como ar condicionados, chuveiros elétricos e bombas para
-            piscina, até a equipamentos mais simples como computadores,
-            equipamentos televisivos e smartphones.
-          </p>
-        </div>
-      </div>
-
-      <div className="">
-        <ServiçosPageItem
-          titulo={'Projeto Hidrossanitário'}
-          imagem={Hidrossanitário}
-        />
-        <div className="flex w-full flex-col items-center justify-center gap-6 p-6 text-black">
-          <p className="w-8/12 p-8 text-justify text-lg font-medium leading-relaxed lg:w-11/12 sm:p-0 2sm:text-xs">
-            Um projeto Hidrossanitário é aquele que engloba toda a distribuição
-            de água fria, água quente, esgoto e água pluvial ao longo da
-            edificação. Ele é essencial para que a água que vem da
-            concessionária chegue até as peças de utilização, como chuveiro,
-            torneiras, etc. Também é fundamental para que os dejetos eliminados
-            pelos ralos e vasos sanitários cheguem até a tubulação de esgoto da
-            rua.
-            <br /> <br />
-            Além da correta distribuição dos equipamentos hidrossanitário que
-            levam água da concessionária até os pontos de saída, e levam o
-            esgoto até a coleta municipal, o projeto hidrossanitário é
-            responsável por evitar problemas nesse tipo de instalação, como
-            falta de pressão nos pontos de água fria/quente, entupimento de
-            esgoto, mal escoamento de água, etc.
-          </p>
-        </div>
-      </div>
-
-      <div className="">
-        <ServiçosPageItem
-          titulo={'Prevenção e Combate à Incêndio (PPCI)'}
-          imagem={PPCI}
-        />
-        <div className="flex w-full flex-col items-center justify-center gap-6 p-6 text-black">
-          <p className="w-8/12 p-8 text-justify text-lg font-medium leading-relaxed lg:w-11/12 sm:p-0 2sm:text-xs">
-            O PPCI (Projeto de Prevenção e Combate a Incêndios) é um documento
-            que sistematiza e detalha os elementos necessários a serem previstos
-            para garantir a segurança dos ocupantes e auxiliar no combate a
-            incêndios. Ele define os espaços e sistemas da edificação para
-            evacuar as pessoas de forma rápida em casos de emergência e iniciar
-            o combate ao fogo em casos de princípios de incêndio.
-            <br /> <br />É necessário, pelas normas brasileiras vigentes, em
-            todas as instalações comerciais, industriais, prédios e locais de
-            concentração de público, além dos edifícios residenciais
-            multifamiliares. O não cumprimento das regulamentações estabelecidas
-            pode acarretar multa.
-          </p>
-        </div>
-      </div>
-
-      <div className="">
-        <ServiçosPageItem
-          titulo={'Orçamento Para Financiamento'}
-          imagem={Orçamento}
-        />
-        <div className="flex w-full flex-col items-center justify-center gap-6 p-6 text-black">
-          <p className="w-8/12 p-8 text-justify text-lg font-medium leading-relaxed lg:w-11/12 sm:p-0 2sm:text-xs">
-            Para que seja realizado o financiamento nas instituições financeiras
-            é necessário o preenchimento de planilhas de orçamento específicas
-            dessas instituições. Essas planilhas são diferentes das planilhas
-            comumente utilizadas na construção civil em obras privadas. A
-            planilha da caixa tem uma série de detalhes que, sem um profissional
-            qualificado, vai haver uma certa dificuldade no aceite pela
-            instituição, ou até mesmo uma reprovação de financiamento.
-            <br /> <br />
-            Nós da Esquare temos profissionais experientes neste tipo de serviço
-            e fazemos com o maior zelo possível para que seu financiamento saia
-            o mais rápido possível.
-          </p>
-        </div>
-      </div>
+    <div className="animate__fadeIn animate__animated flex h-full min-h-screen flex-col items-center justify-center">
+      {
+        serviçosData.map((item, key) => (
+          <div key={key} id={item.titulo}
+            className="flex items-center justify-center flex-col gap-10">
+            <div className="relative flex items-center justify-center w-10/12 overflow-hidden bg-cover">
+              <Image
+                src={item.categoria}
+                alt=""
+                priority={true}
+                height={400}
+                className="h-80 w-screen overflow-hidden bg-cover object-cover brightness-50"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 grid place-items-center">
+                <div className="flex w-[700px] flex-col items-center justify-center text-gray-50 md:w-9/12">
+                  <div className="flex flex-col items-center justify-center text-start">
+                    <h2 className="min-w-fit text-3xl font-extrabold sm:text-center">
+                      {item.titulo}
+                    </h2>
+                    <span className="mb-2 h-3 min-w-fit select-none border-b-2 border-gray-50 text-3xl font-extrabold text-transparent">
+                      {item.titulo}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full flex-col items-center justify-center gap-6 p-6 text-black">
+              <p className="w-8/12 p-8 text-justify text-lg font-medium leading-relaxed lg:w-11/12 sm:p-0 2sm:text-xs">
+                {item.descrição}
+              </p>
+              {
+                item.imagens ?
+                  <div className="flex items-center justify-center flex-wrap min-h-56 w-full">
+                    {
+                      item.imagens.map((image, key) => (
+                        <Image key={key} src={image} alt="" width={300} height={200} />
+                      ))
+                    }
+                  </div>
+                  : null
+              }
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
---
+++ src/data/serviçosData.ts
@@ -0,0 +1,64 @@
+import Orçamento from '@/assets/SectionServiços/Orçamento.jpg'
+import PPCI from '@/assets/SectionServiços/Prevenção-e-Combate-a-Incêndio-(PPCI).jpg'
+import Eletrico from '@/assets/SectionServiços/Projeto-Elétrico.jpg'
+import Arquitetonico from '@/assets/SectionServiços/Projeto-arquitetônico.jpg'
+import Estrutural from '@/assets/SectionServiços/Projeto-estrutural.jpg'
+import Hidrossanitário from '@/assets/SectionServiços/Projetos-Hidrossanitário.png'
+
+
+import { StaticImageData } from 'next/image'
+
+interface serviçosDataTypes {
+  categoria: StaticImageData
+  titulo: string,
+  descrição: string,
+  imagens?: string[],
+}
+
+export const serviçosData: serviçosDataTypes[] = [
+  {
+    categoria: Arquitetonico,
+    titulo: 'Projeto Arquitetônico',
+    descrição: 'O projeto arquitetônico é o primeiro projeto a ser concebido quando se vai construir ou reformar sua casa ou empreendimento. Nele vão estar contidas todas as informações acerca dos desejos do cliente, do ue pode ou não pode ter em seu projeto, além de outros detalhes que possam vir a impactar a concepção do projeto. O projeto arquitetônico abrange várias etapas até a apresentação do projeto executivo final, sendo possível prever nele problemas de execução e facilitar a esma, tornando-se essencial para a garantia de qualidade e segurança na obra.',
+  },
+  {
+    categoria: Estrutural,
+    titulo: 'Projeto Estrutural',
+    descrição: 'O projeto estrutural é o dimensionamento e detalhamento da estrutura necessária para sustentar a edificação, composta por vigas, pilares, lajes e fundação. Por meio de critérios e cálculos específicos, e de acordo com as normas vigentes, o engenheiro define qual o tipo de estrutura mais adequado para o projeto, as dimensões e especificações de cada elemento.   É considerado o projeto mais importante de uma construção, pois é ele que vai garantir a sustentação da edificação.Além de evitar problemas como trincas, rachaduras, rebaixamento de piso, etc.',
+    imagens: [
+      'https://i.imgur.com/3F0cN1l.png',
+      'https://i.imgur.com/DZ72hWA.png',
+      'https://i.imgur.com/Ttb4umc.png',
+    ],
+  },
+  {
+    categoria: Eletrico,
+    titulo: 'Projeto Eletrico',
+    descrição: 'Um projeto elétrico residencial é o conjunto de todas as informações, cálculos, desenhos e detalhes referentes à instalação elétrica residencial, observando todas as normas nacionais vigentes. Essas informações podem ser um diagrama elétrico, tabela de dados, layout, desenhos técnicos, listagem dos componentes da instalação e outros.   O projeto elétrico se beneficia muito de um projeto arquitetônico bem feito.Além de ser extremamente necessário para proteção dos equipamentos ligados na casa, desde equipamentos mais robustos como ar condicionados, chuveiros elétricos e bombas para piscina, até a equipamentos mais simples como computadores, equipamentos televisivos e smartphones.',
+    imagens: [
+      'https://i.imgur.com/PStFQSy.png',
+      'https://i.imgur.com/rL3ZXaR.png',
+      'https://i.imgur.com/Pp34TB6.png',
+    ],
+  },
+  {
+    categoria: Hidrossanitário,
+    titulo: 'Projeto Hidrossanitário',
+    descrição: 'Um projeto Hidrossanitário é aquele que engloba toda a distribuição de água fria, água quente, esgoto e água pluvial ao longo da edificação. Ele é essencial para que a água que vem da concessionária chegue até as peças de utilização, como chuveiro, torneiras, etc. Também é fundamental para que os dejetos eliminados pelos ralos e vasos sanitários cheguem até a tubulação de esgoto da rua.   Além da correta distribuição dos equipamentos hidrossanitário que levam água da concessionária até os pontos de saída, e levam o esgoto até a coleta municipal, o projeto hidrossanitário é responsável por evitar problemas nesse tipo de instalação, como falta de pressão nos pontos de água fria/ quente, entupimento de esgoto, mal escoamento de água, etc.',
+    imagens: [
+      'https://i.imgur.com/fAuSkRi.png',
+      'https://i.imgur.com/hhA5Hu0.png',
+      'https://i.imgur.com/dRi1gMb.png',
+    ],
+  },
+  {
+    categoria: PPCI,
+    titulo: 'Prevenção e Combate à Incêndio (PPCI)',
+    descrição: 'O PPCI (Projeto de Prevenção e Combate a Incêndios) é um documento que sistematiza e detalha os elementos necessários a serem previstos para garantir a segurança dos ocupantes e auxiliar no combate a incêndios. Ele define os espaços e sistemas da edificação para evacuar as pessoas de forma rápida em casos de emergência e iniciar o combate ao fogo em casos de princípios de incêndio.  É necessário, pelas normas brasileiras vigentes, em todas as instalações comerciais, industriais, prédios e locais de concentração de público, além dos edifícios residenciais multifamiliares.O não cumprimento das regulamentações estabelecidas pode acarretar multa.',
+  },
+  {
+    categoria: Orçamento,
+    titulo: 'Orçamento Para Financiamento',
+    descrição: 'Para que seja realizado o financiamento nas instituições financeiras é necessário o preenchimento de planilhas de orçamento específicas dessas instituições. Essas planilhas são diferentes das planilhas comumente utilizadas na construção civil em obras privadas. A planilha da caixa tem uma série de detalhes que, sem um profissional qualificado, vai haver uma certa dificuldade no aceite pela instituição, ou até mesmo uma reprovação de financiamento.   Nós da Esquare temos profissionais experientes neste tipo de serviço e fazemos com o maior zelo possível para que seu financiamento saia o mais rápido possível.',
+  },
+]
