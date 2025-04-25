ed3d82628dcb6f2b51a4631703b8f539f5ef71d5
Filipe-DLL <filipeferreira.new@gmail.com>
2025-04-24 21:39:21 -0300

    Alinhamento do texto e Adição do Botão

--- src/components/SectionDados/index.tsx
+++ src/components/SectionDados/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import sobre from "@/assets/sobre/sobre.png"
+import Link from "next/link";
 
 export default function SectionQuemSomos() {
   return (
@@ -11,30 +12,34 @@ export default function SectionQuemSomos() {
       {/* box */}
       <div className="w-full">
         {/* dados */}
-        <div className="flex h-40 items-center justify-center gap-4 w-full bg-[#0f3090] text-6xl font-black text-[#546fad]">
-          <p className="flex flex-col items-center">
-            +12.000m² <span className="text-lg font-black text-[#d9d9d9]">PROJETADOS</span>
+        <div className="flex h-44 items-center justify-center gap-4 w-full bg-[#0f3090] text-7xl font-black text-[#546fad]">
+          <p className="flex flex-col items-center relative">
+            +12.000m² <span className="text-2xl font-black -mt-5 tracking-widest text-[#d9d9d9]">PROJETADOS</span>
           </p>
-          <p className="flex flex-col items-center">
-            +65 <span className="text-lg font-black text-[#d9d9d9]">PROJETOS ENTREGUES</span>
+          <p className="flex flex-col items-center relative">
+            +65 <span className="text-2xl font-black -mt-5  text-[#d9d9d9]">PROJETOS ENTREGUES</span>
           </p>
-          <p className="flex flex-col items-center">
-            <span className="text-lg font-black text-[#d9d9d9]">ATUAÇÃO EM</span> 10 Cidades
+          <p className="flex flex-col items-center relative">
+            <span className="text-2xl font-black -mb-5 tracking-widest z-10 text-[#d9d9d9]">ATUAÇÃO EM</span> 10 Cidades
           </p>
         </div>
 
         {/* conteúdo */}
         <div className="bg flex items-center justify-center w-full bg-[#d0d4da]">
           {/* texto */}
-          <div className="font-black text-6xl w-2/6 flex justify-end">
+          <div className="flex flex-col font-black text-6xl w-2/6 flex justify-end">
             <p className="text-start">
-              Projetos complementares completos para garantir 
+              Projetos complementares completos para garantir
               <span className="text-black"> eficiência</span>, <span className="text-black">segurança</span> e{" "}
               <span className="text-black">fluidez </span>
               na sua obra.
             </p>
             {/* botão */}
-            {/* <div>Botão</div> */}
+            <Link href={"/sobre"} className="flex h-12">
+              <div className="flex h-full mt-5 p-8 px-12 cursor-pointer items-center justify-center rounded-full bg-[#161c33] text-center text-2xl font-bold text-gray-50 transition-all ease-in-out hover:bg-[#263059]">
+                Saiba mais sobre nós
+              </div>
+            </Link>
           </div>
           {/* imagem */}
           <Image
@@ -43,8 +48,9 @@ export default function SectionQuemSomos() {
             width={500}
             height={500}
             quality={100}
+            unoptimized={true}
             priority={true}
-            className="cursor-pointer w-1/2 -mt-12"
+            className="w-1/2 -mt-12"
           />
         </div>
       </div>
