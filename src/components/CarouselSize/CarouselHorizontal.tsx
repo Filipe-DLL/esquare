05689f05c937dd19073b4228e8d13fda00bcc12b
Filipe-DLL <filipeferreira.new@gmail.com>
2025-05-14 20:33:15 -0300

    carrossel mobile

--- src/components/CarouselSize/CarouselHorizontal.tsx
+++ src/components/CarouselSize/CarouselHorizontal.tsx
@@ -8,47 +8,47 @@ import {
 import { projectData } from "@/data/projectData";
 import Image from "next/image";
 
-export default function CarouselSize() {
+export default function CarouselHorizontal() {
   return (
     <Carousel
       opts={{
-        align: "center",
-        loop: true,
+        align: "start",
       }}
-      className="w-full sm:hidden"
+      orientation="vertical"
+      className="hidden w-full sm:flex"
     >
-      <CarouselContent className="flex items-center gap-10 px-10">
+      <CarouselContent className="h-[650px] w-full">
         {projectData.slice(1, 4).map((item, key) => (
           <CarouselItem
             key={key}
-            className="flex max-w-min items-center justify-center "
+            className="flex h-full w-auto items-center justify-center rounded-lg shadow-md"
           >
-            <div className="flex items-center justify-center rounded-lg bg-[#fcfbff] shadow-md lg:flex-col">
-              <div className="flex w-[500px] items-center justify-center overflow-hidden bg-cover lg:w-auto">
+            <div className="flex flex-col items-center justify-center rounded-lg bg-[#fcfbff]">
+              <div className="flex w-auto items-center justify-center overflow-hidden bg-cover">
                 <Image
                   src={item.imagens[0]}
                   alt=""
                   width={800}
                   height={800}
-                  className="h-[500px] w-full overflow-hidden rounded-l-lg bg-cover object-cover lg:max-h-80 lg:w-full lg:rounded-lg"
+                  className="h-[300px] w-[500px] overflow-hidden rounded-t-lg bg-cover object-cover"
                 />
               </div>
 
-              <div className="flex h-80 flex-col items-start justify-center gap-6 p-5">
-                <h1 className="text-2xl font-bold">
+              <div className="flex w-auto flex-col items-start justify-center gap-6 p-4 px-8 2md:p-4">
+                <h1 className="text-2xl font-bold 2md:text-xl">
                   <span>{item.titulo}</span>
                 </h1>
-                <p>
+                <p className="2md:text-sm">
                   <span className="font-semibold">ABRANGÊNCIA: </span>
                 </p>
-                <p>
+                <p className="2md:text-sm">
                   <span className="font-semibold">ESPECIALIDADE: </span>
                 </p>
-                <p>
+                <p className="2md:text-sm">
                   <span className="font-semibold">ATUAÇÃO: </span>
                 </p>
 
-                <p className="w-96 text-base font-medium  lg:text-justify">
+                <p className="text-base font-medium  lg:text-justify 2md:text-sm">
                   <span className="font-semibold">DESTAQUES: </span>
                   {item.descrição}
                 </p>
@@ -71,5 +71,5 @@ export default function CarouselSize() {
         </h3>
         <ArrowRight className="text-sky-600 transition-all ease-in-out group-hover:text-sky-500 group-hover:opacity-90" />
     </div>
- </Link> */
+</Link> */
 }
--- src/components/SectionProjetos/index.tsx
+++ src/components/SectionProjetos/index.tsx
@@ -1,10 +1,11 @@
 import CarouselSize from "@/components/CarouselSize";
+import CarouselHorizontal from "@/components/CarouselSize/CarouselHorizontal";
 
 export default function SectionProjetos() {
   return (
     <section
       id="Projetos"
-      className="flex h-full w-screen flex-col items-center justify-center overflow-hidden bg-[#0e2c88] px-32 sm:p-0"
+      className="flex h-full w-screen flex-col items-center justify-center overflow-hidden bg-[#0e2c88] px-32 md:p-12 2md:px-4"
     >
       {/* Titulo */}
       <h1 className="m-20 flex items-center justify-center self-start text-8xl font-black text-[#526ca7] md:self-center">
@@ -12,6 +13,7 @@ export default function SectionProjetos() {
       </h1>
 
       <CarouselSize />
+      <CarouselHorizontal />
     </section>
   );
 }
