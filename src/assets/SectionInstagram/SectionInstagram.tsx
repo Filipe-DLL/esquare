6b7258364a24e45b78a3cea0e3d9f77a6ca39c4f
Filipe-DLL <filipeferreira.new@gmail.com>
2026-04-14 22:07:30 -0300

    responsividade no instagram

--- src/assets/SectionInstagram/SectionInstagram.tsx
+++ src/assets/SectionInstagram/SectionInstagram.tsx
@@ -2,11 +2,12 @@ export default function SectionInstagram() {
   return (
     <div className="flex items-center justify-center max-w-5xl w-full">
       <iframe
-        //src="https://be310cfd84404822a582b8af6450611f.elf.site"
+       // src="https://be310cfd84404822a582b8af6450611f.elf.site"
         src="https://64db68c8e44448a29883dc220f0998cc.elf.site"
-        width='75%' height='1200'
+        width='700'
+        height='750'
         className="w-4/5 sm:w-full overflow-x-hidden overflow-hidden">
-      </iframe>
+       </iframe>
     </div>
   )
 }
