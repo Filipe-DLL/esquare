bbc063cede1e978b9a9a439ebd682315f74754ba
Filipe-DLL <filipeferreira.new@gmail.com>
2026-04-15 23:15:07 -0300

    responsividade do instagram

--- src/components/SectionContatos/index.tsx
+++ src/components/SectionContatos/index.tsx
@@ -53,10 +53,11 @@ export default function SectionContatos() {
           <div className="md relative flex h-[700px] w-full max-w-[700px] flex-col items-center justify-center overflow-hidden">
             <iframe
               src="https://64db68c8e44448a29883dc220f0998cc.elf.site"
+              // src="https://be310cfd84404822a582b8af6450611f.elf.site"
               width="700"
               height="796"
-              className="-mb-10"
-            ></iframe> 
+              className="sm:w-96 2md:w-auto -mb-10 overflow-x-hidden overflow-hidden">
+            '</iframe> 
             <Link
               href={"https://www.instagram.com/esquareengenharia/"}
               className="absolute bottom-0 flex items-center justify-center"
